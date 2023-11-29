da5e5e51e6747b08825e853c64f3e34fa0b03d7a
mim1cc <135341726+mim1cc@users.noreply.github.com>
2023-11-29 18:06:50 +0300

    commit

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
-class Example(QWidget):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-    def initUI(self):
         self.setGeometry(300, 300, 200, 200)
         self.setWindowTitle('123')
         self.btn = QPushButton('Draw', self)
@@ -21,27 +18,27 @@ class Example(QWidget):
 
     def paintEvent(self, event):
         if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
+            painter = QPainter()
+            painter.begin(self)
+            self.draw_flag(painter)
+            painter.end()
 
     def paint(self):
         self.do_paint = True
         self.repaint()
 
-    def draw_flag(self, qp):
+    def draw_flag(self, painter):
         a = random.randint(10, 100)
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
-        qp.setBrush(QColor(r, g, b))
-        qp.drawEllipse(random.randint(0, 200), random.randint(0, 200), a, a)
+        painter.setBrush(QColor(r, g, b))
+        painter.drawEllipse(random.randint(0, 200), random.randint(0, 200), a, a)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Window()
     ex.show()
     sys.exit(app.exec())
 
